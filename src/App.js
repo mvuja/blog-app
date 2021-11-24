0324406131b46bef311cad78d42b0adf1409c07f
Marko Vujanovic <markowujanovic@gmail.com>
2021-11-24 18:49:50 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import ButtonComponent from './components/Button/Button';
+import Main from './components/Main/Main';
+import Card from './components/Card/Card';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
 
 
     return (
-        <div><Button variant="contained">Hello World</Button></div>
+        <div>
+            <Navbar />
+            <Hero />
+            <ButtonComponent />
+            <Main />
+            <Card />
+            <Footer />
+        </div>
         
     )
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './hero.modules.css'
+
+const Hero = () => {
+
+    return (
+        <div className="hero">Hello Hero</div>
+    )
+}
+
+
+export default Hero
